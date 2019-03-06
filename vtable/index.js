928d97abc42a4356a13fef8214299f71c85a4e5a
Neverslave <1029110566@qq.com>
2019-03-06 21:23:52 +0800

    2

--- vtable/index.js
+++ vtable/index.js
@@ -1,10 +1,56 @@
 var app =new Vue({
     el:'#app',
     data:{
-        columns:{
+        columns:[
+            {
+                title:'姓名',
+                    key:
+                'name'
+            },
+            {
+                title:'年龄',
+                key: 'age',
+                sortable:'true'
+            },
+            {
+                title:'生日',
+                key:'birthday',
+                sortable:true
+            },
+            {
+                title:'地址',
+                key:'address'
+            }
 
+        ],
+        data:[{
+            name:'王小明',
+            age:18,
+            birthday:'1999-02-21',
+            address:'北京市朝阳区芍药居路'
         },
-        data:{
+            {
+                name:'赵小妮',
+                age:19,
+                birthday:'1998-08-21',
+                address:'北京市朝阳区康居路'
+            },
+            {
+                name:'李晓红',
+                age:30,
+                birthday:'1987-11-10',
+                address:'上海市浦东新区世纪大道'
+            }
+        ]
+    },
+    methods:{
+        handleAddData:function () {
+            this.data.push({
+                name:'刘晓天',
+                age:19,
+                birthday:'1998-05-30',
+                address:'北京市东城区东直门'
+            })
 
         }
     }
--- vtable/vtable.js
+++ vtable/vtable.js
@@ -1,9 +1,9 @@
-Vue.component('vTable',{
+Vue.component('v-table',{
     props:{
         columns:{
             type:Array,
             default:function () {
-                return[]
+                return [];
             }
         },
         data:{
@@ -19,8 +19,8 @@ Vue.component('vTable',{
             currentData:[]
         }
     },
-    method:{
-        makecolumns:function () {
+    methods:{
+        makeColumns:function () {
             this.currentColumns.map(function (col,index) {
                 col._sortType='normal' ;//增加一个字段标识当前的排序状态
                 col._index = index ;//增加一个字段标识当前列在数组中的索引后续使用
@@ -34,12 +34,28 @@ Vue.component('vTable',{
                 return row;
             })
 
+        },
+        handleSortByAsc:function (index) {
+            var key =this.currentColumns[index].key;
+            this.currentColumns.forEach(function (col) {
+                col._sortType='normal'
+            });
+            this.currentColumns[index]._sortType='asc';
+            this.currentData.sort(function (a, b) {
+                return a[key]>b[key]?1:-1;
+            })
+        },
+        handleSortByDesc:function (index) {
+            var key =this.currentColumns[index].key;
+            this.currentColumns.forEach(function (col) {
+                col._sortType='normal'
+            });
+            this.currentColumns[index]._sortType='desc';
+            this.currentData.sort(function (a, b) {
+                return a[key]>b[key]?-1:1;
+            })
+
         }
-    },
-    mounted(){
-        //初始化是调用
-        this.makecolumns();
-        this.makeData();
     },
     render:function (h) {
        var _this =this;
@@ -78,8 +94,43 @@ Vue.component('vTable',{
                ths.push(h('th'),col.title);
 
            }
-       })
+       });
 
+       var trs = [];
+       this.currentData.forEach(function (row) {
+           var tds=[];
+           _this.currentColumns.forEach(function (cell) {
+               tds.push(h('td',row[cell.key]));
+           });
+           trs.push(h('tr',tds))
+       });
+        return h('table',[
+            h('thead',[
+                h('tr',ths)
+            ]),
+            h('tbody',trs)
+        ])
+
+    },
+    watch:{
+        data:function () {
+            this.makeData();
+        var sortedColumn = this.currentColumns.filter(function (col) {
+            return col._sortType!=='normal'
+        });
+        if(sortedColumn.length>0){
+            if(sortedColumn[0]._sortType==='asc'){
+                this.handleSortByAsc(sortedColumn[0]._index);
+            }else{
+                this.handleSortByDesc(sortedColumn[0]._index);
+            }
+            }
+        }
+    },
+
+    mounted (){
+        //初始化是调用
+        this.makeColumns();
+        this.makeData();
     }
-
-})
+});
